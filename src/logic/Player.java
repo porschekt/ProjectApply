ab152091e4303f6144035db745c40fdccbf7c955
Kongpobpisit Thermprateep <porsche.kt@hotmail.com>
2018-05-23 21:39:55 +0700

    a

--- src/logic/Player.java
+++ src/logic/Player.java
@@ -30,7 +30,7 @@ public class Player extends CollidableEntity implements IRenderable {
 
 	public Player(GameLogic gameLogic) {
 		// TODO Auto-generated constructor stub
-		super(2500, 7);
+		super(2500, 6);
 		this.originalHp = this.hp;
 		this.z = 0;
 
